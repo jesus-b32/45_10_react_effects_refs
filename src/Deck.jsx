770696c92508630ccd344362425e80dfa3eb9cea
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-07-30 18:42:15 -0700

    completed part one and two

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -2,13 +2,16 @@ import React, {useState, useEffect, useRef} from "react";
 import Card from "./Card";
 
 
-
 function Deck () {
     const deckIdRef = useRef();
 
     //a card will be an object that will hold following data: suit, value, image
     const [cards, setCards] = useState([]);
 
+    //used to determine if deck of cards is being shuffled
+    const [shuffling, setShuffling] = useState(false);
+
+    // will shuffle only once at react app start up
     useEffect (function shuffleCardsWhenMounted () {
         async function shuffleCards() {
             const response = await fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
@@ -21,23 +24,40 @@ function Deck () {
     //update state by adding current cards and newCard, made from form API, to setState
     function handleClick () {
         async function fetchCard () {
-            console.log(deckIdRef.current);
             const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckIdRef.current}/draw/?count=1`);
             const data = await response.json();
             setCards(cards => [...cards, data.cards[0]]);
         }
         fetchCard();
-        // const newCard = fetchCard();
-        // setCards(cards => [...cards, newCard]);
     }
 
+    // reshuffle the current deck and clear the screen of cards
+    function handleshuffle () {
+        async function ReshuffleCards () {
+            setShuffling(true);
+            await fetch(`https://deckofcardsapi.com/api/deck/${deckIdRef.current}/shuffle/`);
+            setCards([]);
+            setShuffling(false);
+        }
+        ReshuffleCards();
+    }
+
+    //make card components from cards state
     const currentCardComponents = cards.map(card => (
-        <Card src={card.image}/>
+        <Card 
+        key={card.code}
+        src={card.image}/>
     ));
 
     return (
         <div>
-            <button onClick={handleClick}>New Card</button>
+            {shuffling ? 
+            <h2>Shuffling Cards</h2> :
+            <button onClick={handleshuffle}>Reshuffle Cards</button>}
+
+            {cards.length < 52 ? 
+            <button onClick={handleClick}>New Card</button> :
+            <h2>No More Cards</h2>}
             {currentCardComponents}
         </div>
 
